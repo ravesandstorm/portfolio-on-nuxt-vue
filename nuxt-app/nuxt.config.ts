8eba6897128c05d51e91bd0fd01b6112311d73fe
Satvik <Satvik.Beura@gmail.com>
2025-09-27 17:40:51 +0530

    update for deployment fix

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   // GLOBAL CSS
-  css: ['~/assets/main.css']
+  css: ['~/assets/main.css'],
+  app: {
+    baseURL: process?.env.NUXT_APP_BASE_URL || '/'  // fallback to root
+  }
 })
